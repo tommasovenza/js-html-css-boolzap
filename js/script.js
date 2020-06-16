0ae5e1a63b24d36e41da79ffa564cd9ea34b0654
Tommaso Venza <tommasovenza@gmail.com>
2020-06-17 00:06:34 +0200

    aggiunta pressione tasto invio

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     $('#btn').click(function (event) {
 
-        
+
         // al click della tasto microfono(per ora), fa comparire la risposta in verde presa da citazioni varie
 
         // con set timeout faccio rispondere dopo 3 secondi, ovvero faccio apparire il balloon dopo 3 secondi
@@ -31,14 +31,41 @@ $(document).ready(function () {
 
     });
 
+
+    // evento pressione tasto enter
+    $(document).keypress(function (event) {
+
+        if (event.which == 13) {
+
+            var inputKeypress = $("#input").val();
+            var templateInputKeypress = $('.template .quote-input').clone();
+            templateInputKeypress.children('.quote-text').text(inputKeypress);
+
+            $('#chat').append(templateInputKeypress);
+
+            // svuoto al keypress il campo input
+            $('#input').val('');
+
+            setTimeout(function () {
+
+                var quote2 = getQuote();
+
+                var template2 = $('.template .quote').clone();
+
+                template2.children('.quote-text').text(quote2);
+
+                $('#chat').append(template2);
+
+            }, 3000);
+        }
+
+    });
+
 }); //end document ready
-
-
-
 
 function getQuote() {
 
-    var quotes = ['Molto bene, grazie. E tu?', 'dai, non c\'è male', 'yessss', 'oook', 'ciao', 'sono giornate toste'];
+    var quotes = ['Molto bene, grazie. E tu?', 'de', 'dai, non c\'è male', 'de ma de', 'yessss', 'oook', 'ciao', 'sono giornate difficili', 'oooh allora??', 'che fai?', ' che si dice??', 'eh lo so' ];
 
     return quotes[getRandomInt(0, (quotes.length))];
 }
