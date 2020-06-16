6a3ab374b293dd72baa6543f0af84998d1659c83
Tommaso Venza <tommasovenza@gmail.com>
2020-06-16 20:05:30 +0200

    prima prova chat

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
-var arrayFrasi = [ 'Molto bene, grazie. E tu?', 'dai, non c\'è male', 'yessss', 'oook', 'ciao', 'sono giornate toste'];
+
+
+
+$(document).ready(function() {
+
+    $('#btn').click(function(event){
+    
+        var quote = getQuote();
+
+        console.log(quote);
+    });
+});
+
+  
+
+
+function getQuote() {
+
+    var quotes = [ 'Molto bene, grazie. E tu?', 'dai, non c\'è male', 'yessss', 'oook', 'ciao', 'sono giornate toste'];
+
+    return quotes[getRandomInt(0, (quotes.length))];
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
+  }
 
