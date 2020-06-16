9f87f8d352a2cd15869af46e92fb4a038ac9a3de
Tommaso Venza <tommasovenza@gmail.com>
2020-06-16 20:41:25 +0200

    implementato setTimeout

--- js/script.js
+++ js/script.js
@@ -1,22 +1,34 @@
+$(document).ready(function () {
+
+    $('#btn').click(function (event) {
+
+        // al click della tasto microfono(per ora), fa comparire la risposta in verde presa da citazioni varie
+
+        setTimeout(function () {
+
+            var quote = getQuote();
+
+            console.log(quote);
+
+            var template = $('.template .quote').clone();
+
+            template.children('.quote-text').text(quote);
+
+            $('#chat').append(template);
+        }, 3000);
 
 
 
-$(document).ready(function() {
 
-    $('#btn').click(function(event){
-    
-        var quote = getQuote();
-
-        console.log(quote);
     });
-});
+}); //end document ready
 
-  
+
 
 
 function getQuote() {
 
-    var quotes = [ 'Molto bene, grazie. E tu?', 'dai, non c\'è male', 'yessss', 'oook', 'ciao', 'sono giornate toste'];
+    var quotes = ['Molto bene, grazie. E tu?', 'dai, non c\'è male', 'yessss', 'oook', 'ciao', 'sono giornate toste'];
 
     return quotes[getRandomInt(0, (quotes.length))];
 }
@@ -25,5 +37,4 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
-  }
-
+}
